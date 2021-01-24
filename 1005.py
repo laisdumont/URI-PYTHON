c31968a1859ff6d06b1fd80429afa88cc7525ee1
Ana Dumont <laisiinha18@gmail.com>
2021-01-23 23:32:58 -0300

    Média 1

---
+++ 1005.py
@@ -0,0 +1,13 @@
+"""
+    Autora: laisdumont
+    Data: 23/01/21
+    Desafio: 1005 - Média 1
+"""
+# -*- coding: utf-8 -*-
+
+A = float(input())
+B = float(input())
+
+m = (A*0.31818181 + B*0.68181818)
+
+print(f'MEDIA = {m:.5f}')
