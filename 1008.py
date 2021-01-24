d2bb46eac1e94e2dc6772fe9b1578c2ca12fc727
Ana Dumont <laisiinha18@gmail.com>
2021-01-23 23:47:38 -0300

    Salário

---
+++ 1008.py
@@ -0,0 +1,13 @@
+"""
+    Autora: laisdumont
+    Data: 23/01/21
+    Desafio: 1008 - Salário
+"""
+# -*- coding: utf-8 -*-
+
+nfunc = int(input())
+horas = int(input())
+valor = float(input())
+
+print(f'NUMBER = {nfunc}')
+print(f'SALARY = U$ {(valor*horas):.2f}')
