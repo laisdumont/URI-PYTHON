ab642db821b9975148c0eaf1c6ee75e852dc3432
Ana Dumont <laisiinha18@gmail.com>
2021-01-23 23:37:02 -0300

    Média 2

---
+++ 1006.py
@@ -0,0 +1,14 @@
+"""
+    Autora: laisdumont
+    Data: 23/01/21
+    Desafio: 1006 - Média 2
+"""
+# -*- coding: utf-8 -*-
+
+A = float(input())
+B = float(input())
+C = float(input())
+
+m = A*0.2 + B*0.3 + C*0.5
+
+print(f'MEDIA = {m:.1f}')
