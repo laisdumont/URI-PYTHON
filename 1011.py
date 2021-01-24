dd5dc0e6b7693efcc2087920bf2f50d279a09e78
Ana Dumont <laisiinha18@gmail.com>
2021-01-24 00:08:48 -0300

    Esfera

---
+++ 1011.py
@@ -0,0 +1,12 @@
+"""
+    Autora: laisdumont
+    Data: 24/01/21
+    Desafio: 1011 - Esfera
+"""
+# -*- coding: utf-8 -*-
+
+raio = float(input())
+
+volume = (4/3) * 3.14159 * pow(raio, 3)
+
+print(f'VOLUME = {volume:.3f}')
