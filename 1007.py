4e1f29dfe9cd9815a06f0b4b84a5146b2b3e36bd
Ana Dumont <laisiinha18@gmail.com>
2021-01-23 23:39:56 -0300

    Diferença

---
+++ 1007.py
@@ -0,0 +1,15 @@
+"""
+    Autora: laisdumont
+    Data: 23/01/21
+    Desafio: 1007 - Diferença
+"""
+# -*- coding: utf-8 -*-
+
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+
+d = (A*B) - (C*D)
+
+print(f'DIFERENCA = {d}')
