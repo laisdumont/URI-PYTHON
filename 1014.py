91f609fe89d80b4f87de841362a048312b491724
Ana Dumont <laisiinha18@gmail.com>
2021-01-24 00:43:32 -0300

    Consumo

---
+++ 1014.py
@@ -0,0 +1,11 @@
+"""
+    Autora: laisdumont
+    Data: 24/01/21
+    Desafio: 1014 - Consumo
+"""
+# -*- coding: utf-8 -*-
+
+distância = int(input())
+comb_gasto = float(input())
+
+print(f'{(distância/comb_gasto):.3f} km/l')
