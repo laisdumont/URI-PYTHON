bcf9ed78577b5e7464f9a263112cc45955c7bec2
Ana Dumont <laisiinha18@gmail.com>
2021-01-28 13:19:15 -0300

    Sorte simples

---
+++ 1042.py
@@ -0,0 +1,14 @@
+"""
+    Data: 24/01/21
+    Desafio: 1042 - Sort simples
+"""
+# -*- coding: utf-8 -*-
+
+v1, v2, v3 = list(map(int, input().split()))
+lista = [v1, v2, v3]
+
+for c in sorted(lista):
+    print(c)
+print()
+for c in lista:
+    print(c)
